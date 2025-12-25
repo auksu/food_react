3737ec6fd1eab506175ca65752c0734f126a2a98
auksu <newmail@users.noreply.github.com>
2025-12-25 18:26:56 +0900

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ git init
 git add .
 git commit -m "initial commit"
 git branch -M main
-git remote add origin https://github.com/<username>/<repo>.git
+git remote add origin https://github.com/auksu/food_react.git
 git push -u origin main
 
 <gh-pages>
