5993fd79e7221a908b9040567a4eebac2f82bb2e
auksu <newmail@users.noreply.github.com>
2025-12-25 18:26:36 +0900

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,17 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+/* 
+<main>
+git init
+git add .
+git commit -m "initial commit"
+git branch -M main
+git remote add origin https://github.com/<username>/<repo>.git
+git push -u origin main
+
+<gh-pages>
+npm run build
+npm run deploy
+*/
